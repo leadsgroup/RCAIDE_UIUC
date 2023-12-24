aff80db65f1371d834e987e3fb1b79ca6f7a71da
Matthew Clarke <mclarke2@stanford.edu>
2023-12-24 15:43:20 -0800

    removal of fuel selector

--- RCAIDE/Energy/Propulsion/Modulators/__init__.py
+++ RCAIDE/Energy/Propulsion/Modulators/__init__.py
@@ -10,8 +10,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 ## @ingroup Energy
-from .Electronic_Speed_Controller                  import Electronic_Speed_Controller
-from .Fuel_Selector                                import Fuel_Selector
+from .Electronic_Speed_Controller                  import Electronic_Speed_Controller 
 from Legacy.trunk.S.Components.Energy.Distributors import HTS_DC_Dynamo_Basic
 from Legacy.trunk.S.Components.Energy.Distributors import HTS_DC_Supply 
 
