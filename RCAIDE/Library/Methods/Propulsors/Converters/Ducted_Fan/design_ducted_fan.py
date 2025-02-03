f6d6a4cc21c3d201bee0eab183b7426e7ac41766
Matthew Clarke <mclarke2@stanford.edu>
2025-02-02 23:15:47 -0600

    Update design_ducted_fan.py

--- RCAIDE/Library/Methods/Propulsors/Converters/Ducted_Fan/design_ducted_fan.py
+++ RCAIDE/Library/Methods/Propulsors/Converters/Ducted_Fan/design_ducted_fan.py
@@ -82,7 +82,7 @@ def design_ducted_fan(ducted_fan, dfdc_bin_name = 'dfdc', new_regression_results
 
         Q    = (P_EM / omega)[0][0]
                 
-        ducted_fan.cruise.design_power              = P_EM 
+        ducted_fan.cruise.design_power              = P_EM[0][0] 
         ducted_fan.cruise.design_efficiency         = eta_p 
         ducted_fan.cruise.design_torque             = Q 
         ducted_fan.cruise.design_thrust_coefficient = C_t  
